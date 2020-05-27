8d97590ea6e2997d96c03127fd954dbb24f68d2d
Jeff Kee <jeffkhm@gmail.com>
2020-05-27 15:56:02 -0700

    update US part

--- driving.py
+++ driving.py
@@ -1,7 +1,13 @@
 country = input('What is your nationality:')
 age = int(input('Enter your age:'))
-if country == 'malaysia':
+if country == 'malaysia':  # if the country isn't malaysia it won't execute 
 	if age >= 18:
-		print('You can apply driving lincese')
+		print('You can apply driving license')
 	else:
-		print('You cannot apply driving lincese')
+		print('You cannot apply driving license')
+elif country == 'united state':
+     if age >= 16:
+     	print('You can apply driving license')
+     else:
+     	print('You cannot apply driving license') 
+     		
