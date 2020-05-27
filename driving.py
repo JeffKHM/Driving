04de23cf1bc0d792dea31e3f4f5a123e0c32849a
Jeff Kee <jeffkhm@gmail.com>
2020-05-27 15:39:02 -0700

    update malaysia

---
+++ driving.py
@@ -0,0 +1,7 @@
+country = input('What is your nationality:')
+age = int(input('Enter your age:'))
+if country == 'malaysia':
+	if age >= 18:
+		print('You can apply driving lincese')
+	else:
+		print('You cannot apply driving lincese')
